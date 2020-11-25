b4f832f30e93c17c463f8e417eabca36045b1fa5
Shunottara-Sarade <74823403+Shunottara-Sarade@users.noreply.github.com>
2020-11-25 20:41:14 +0530

    Add files via upload

---
+++ Java Lab Solutions/Lab7/src/com/cg/eis/exception/EmployeeException.java
@@ -0,0 +1,20 @@
+package com.cg.eis.exception;
+public class EmployeeException extends Exception{
+	private String message;
+	
+	public EmployeeException() {
+		super();
+	}
+
+	public EmployeeException(String message) {
+		super();
+		this.message = message;
+	}
+
+	@Override
+	public String getMessage() {
+		return this.message+ "\n"+super.getMessage();
+	}
+	
+	
+}
